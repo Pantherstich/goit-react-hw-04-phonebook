e901ceb257487691d7e2ae18d7512a4399730c92
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-12-08 21:13:18 +0200

    hook power

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,70 +1,132 @@
-import React, { Component } from 'react';
 import { Button, ContForm, Input, Label } from './ContactForm.styled';
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
-const nameInputId = nanoid();
-const numberInputId = nanoid();
+export const ContactForm = ({ addContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [id, setId] = useState('');
+  let nameInputId = nanoid();
+  let numberInputId = nanoid();
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-    id: '',
-  };
-
-  nameInputId = nanoid();
-  numberInputId = nanoid();
-
-  onChangeFilter = event => {
-    const { name, value, id } = event.currentTarget;
+  const onChangeFilter = event => {
+    const { name, value } = event.currentTarget;
     if (
       (name === 'name' && /^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ' ]*$/.test(value)) ||
       (name === 'number' && /^[0-9-]*$/.test(value))
     ) {
-      this.setState({ [name]: value, id });
+      if (name === 'name') {
+        setName(value);
+      } else if (name === 'number') {
+        setNumber(value);
+      }
+      setId(nanoid());
     }
   };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.addContact(this.state);
-    this.reset();
+    addContact({ name, number, id });
+    reset();
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
-    // this.nameInputId = nanoid();
+  const reset = () => {
+    setName('');
+    setNumber('');
+    nameInputId = nanoid();
   };
-  render() {
-    return (
-      <ContForm onSubmit={this.handleSubmit}>
-        <Label htmlFor={nameInputId}>
-          Name
-          <Input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.onChangeFilter}
-            id={nameInputId}
-            required
-          />
-        </Label>
-        <Label htmlFor={numberInputId}>
-          Number
-          <Input
-            type="tel"
-            name="number"
-            value={this.state.number}
-            onChange={this.onChangeFilter}
-            id={numberInputId}
-            required
-          />
-        </Label>
-        <Button type="submit">Add contact</Button>
-      </ContForm>
-    );
-  }
-}
+
+  return (
+    <ContForm onSubmit={handleSubmit}>
+      <Label htmlFor={nameInputId}>
+        Name
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          onChange={onChangeFilter}
+          id={nameInputId}
+          required
+        />
+      </Label>
+      <Label htmlFor={numberInputId}>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={onChangeFilter}
+          id={numberInputId}
+          required
+        />
+      </Label>
+      <Button type="submit">Add contact</Button>
+    </ContForm>
+  );
+};
+
+// const nameInputId = nanoid();
+// const numberInputId = nanoid();
+
+// export class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//     id: '',
+//   };
+
+//   nameInputId = nanoid();
+//   numberInputId = nanoid();
+
+//   onChangeFilter = event => {
+//     const { name, value, id } = event.currentTarget;
+//     if (
+//       (name === 'name' && /^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ' ]*$/.test(value)) ||
+//       (name === 'number' && /^[0-9-]*$/.test(value))
+//     ) {
+//       this.setState({ [name]: value, id });
+//     }
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     this.props.addContact(this.state);
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({
+//       name: '',
+//       number: '',
+//     });
+//     // this.nameInputId = nanoid();
+//   };
+//   render() {
+//     return (
+//       <ContForm onSubmit={this.handleSubmit}>
+//         <Label htmlFor={nameInputId}>
+//           Name
+//           <Input
+//             type="text"
+//             name="name"
+//             value={this.state.name}
+//             onChange={this.onChangeFilter}
+//             id={nameInputId}
+//             required
+//           />
+//         </Label>
+//         <Label htmlFor={numberInputId}>
+//           Number
+//           <Input
+//             type="tel"
+//             name="number"
+//             value={this.state.number}
+//             onChange={this.onChangeFilter}
+//             id={numberInputId}
+//             required
+//           />
+//         </Label>
+//         <Button type="submit">Add contact</Button>
+//       </ContForm>
+//     );
+//   }
+// }
